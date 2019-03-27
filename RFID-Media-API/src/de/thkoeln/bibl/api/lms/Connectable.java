0cfd2538e7da37644ec2a386373000cbc2ca3b82
Evgeny Kopylov <evgeny.kopylov@th-koeln.de>
2019-03-27 13:23:38 +0100

    Add -> //Initialize main window

--- RFID-Media-API/src/de/thkoeln/bibl/api/lms/Connectable.java
+++ RFID-Media-API/src/de/thkoeln/bibl/api/lms/Connectable.java
@@ -10,42 +10,42 @@ import java.io.OutputStream;
  * 
  */
 public interface Connectable {
-	
+
 	/**
 	 * Opens a connection to the LMS
 	 * 
 	 * @throws LMSException if an error occurred while opening the connection
 	 */
 	public void open() throws LMSException;
-	
+
 	/**
 	 * Closes the connection to the LMS
 	 * 
 	 * @throws LMSException if an error occurred while closing the connection
 	 */
 	public void close() throws LMSException;
-	
+
 	/**
 	 * Returns an input stream for reading from the LMS connection
 	 * 
 	 * @return an readable input stream
 	 */
 	public InputStream getInputStream();
-	
+
 	/**
 	 * Returns an output stream for writing to the LMS connection
 	 * 
 	 * @return an writable output stream
 	 */
 	public OutputStream getOutputStream();
-	
+
 	/**
 	 * Returns the character encoding of the connection
 	 * 
 	 * @return a string representation of the charset encoding
 	 */
 	public String getEncoding();
-	
+
 	/**
 	 * Returns the connection status
 	 * 
